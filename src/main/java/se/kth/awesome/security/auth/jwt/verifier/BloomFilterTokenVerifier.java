c9bf13e87b64ea5fdd9d82fd6ba768eb19416c19
ArminMa <majaris@kth.se>
2016-11-21 13:17:43 +0100

    added logger to the classes

--- src/main/java/se/kth/awesome/security/auth/jwt/verifier/BloomFilterTokenVerifier.java
+++ src/main/java/se/kth/awesome/security/auth/jwt/verifier/BloomFilterTokenVerifier.java
@@ -1,6 +1,9 @@
 package se.kth.awesome.security.auth.jwt.verifier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import se.kth.awesome.SpringbootSecurityJwtApplication;
 
 /**
  * BloomFilterTokenVerifier
@@ -13,8 +16,11 @@ import org.springframework.stereotype.Component;
 //TODO verify token
 @Component
 public class BloomFilterTokenVerifier implements TokenVerifier {
+    private final Logger logger2 = LoggerFactory.getLogger(getClass());
     @Override
     public boolean verify(String jti) {
+        logger2.error("\n\n"+ SpringbootSecurityJwtApplication.steps++ +" ---------- BloomFilterTokenVerifier.verify ----------\n");
+
         return true;
     }
 }
